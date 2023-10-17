f0d91d47d9c526a8d5905b319c0532e67fd80399
Egirahayu <mohammadegirahayu@gmail.com>
2023-10-18 02:49:20 +0700

    Example project

---
+++ php/tubes_pw/config.php
@@ -0,0 +1,10 @@
+<?php
+// Mohamad Egi Rahayu
+// Universitas Pasundan
+// Full Stack 01
+
+$hostName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = 'pw_tubes_203040045';
+$conn = new mysqli($hostName, $userName, $password, $dbName);
---
+++ php/tubes_pw/index.php
@@ -0,0 +1,62 @@
+<?php
+// Mohamad Egi Rahayu
+// Universitas Pasundan
+// Full Stack 01
+
+include "config.php";
+$anime = mysqli_query($conn, "SELECT * FROM anime");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- css materialize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+  <link rel="shortcut icon" href="ER.png">
+  <title>Tubes Pemrograman Web</title>
+
+  <style>
+    body {
+      background-image: url(img/unsplash.jpg);
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <h3 class="center">Main Character (MC) Anime</h3>
+
+
+    <table class="striped">
+      <tr class="orange darken-2">
+        <th class="center">No</th>
+        <th class="center">Image</th>
+        <th>Nama MC</th>
+        <th>Anime</th>
+        <th>Pengarang</th>
+        <th class="center">Rilis</th>
+      </tr>
+
+      <?php $i = 1; ?>
+
+      <?php foreach ($anime as $anm) : ?>
+        <tr class="grey darken-3 white-text">
+          <td class="center"><?= $i; ?></td>
+          <td class="center"><img src="img/<?= $anm['img']; ?>" width="100px"></td>
+          <td><?= $anm['nama']; ?></td>
+          <td><?= $anm['anime']; ?></td>
+          <td><?= $anm['pengarang']; ?></td>
+          <td class="center"><?= $anm['rilis']; ?></td>
+        </tr>
+        <?php $i++; ?>
+      <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
