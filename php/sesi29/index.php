39d44415704bfc7bb13e9095f1ae2e2bbf0d4ec0
Egirahayu <mohammadegirahayu@gmail.com>
2023-10-26 22:08:36 +0700

    Update session 29

--- php/sesi29/index.php
+++ php/sesi29/index.php
@@ -15,8 +15,8 @@ $anime = mysqli_query($conn, "SELECT * FROM anime");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- CSS Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- css materialize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
   <link rel="shortcut icon" href="img/ER.png">
   <title>Main Character Anime</title>
@@ -25,45 +25,45 @@ $anime = mysqli_query($conn, "SELECT * FROM anime");
     body {
       background-image: url(img/unsplash.jpg);
     }
+
+    .add {
+      margin-bottom: 10px;
+    }
   </style>
 </head>
 
 <body>
   <div class="container">
-    <h3 class="text-center p-3">Main Character (MC) Anime</h3>
+    <h3 class="center">Main Character (MC) Anime</h3>
 
-    <table class="table table-hover">
-      <tr class="table-warning">
-        <th class="text-center">No</th>
-        <th class="text-center">Opsi</th>
-        <th class="text-center">Image</th>
+    <table class="striped">
+      <tr class="orange darken-2">
+        <th class="center">No</th>
+        <th class="center">Image</th>
         <th>Nama MC</th>
         <th>Anime</th>
         <th>Pengarang</th>
-        <th class="text-center">Rilis</th>
+        <th class="center">Rilis</th>
       </tr>
 
       <?php $i = 1; ?>
 
       <?php foreach ($anime as $anm) : ?>
         <tr class="grey darken-3 white-text">
-          <td class="text-center table-light"><?= $i; ?></td>
-          <td class="text-center table-light">
-            <a href="edit.php?id=<?= $anm['id']; ?>" class="btn btn-primary">Edit</a>
-            <a href="delete.php?id=<?= $anm['id']; ?>" class="btn btn-danger" onclick="return confirm('Hapus Data?')">Delete</a>
-          </td>
-          <td class="text-center table-light"><img src="img/<?= $anm['img']; ?>" width="100px"></td>
-          <td class="table-light"><?= $anm['nama']; ?></td>
-          <td class="table-light"><?= $anm['anime']; ?></td>
-          <td class="table-light"><?= $anm['pengarang']; ?></td>
-          <td class="text-center table-light"><?= $anm['rilis']; ?></td>
+          <td class="center"><?= $i; ?></td>
+          <td class="center"><img src="img/<?= $anm['img']; ?>" width="100px"></td>
+          <td><?= $anm['nama']; ?></td>
+          <td><?= $anm['anime']; ?></td>
+          <td><?= $anm['pengarang']; ?></td>
+          <td class="center"><?= $anm['rilis']; ?></td>
         </tr>
         <?php $i++; ?>
       <?php endforeach; ?>
     </table>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!-- script materialize -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
